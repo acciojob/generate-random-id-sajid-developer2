b61c4c65f6c8ccade1a6275a4da604d85cfc3600
Md Sajid <sajidcse202024@gmail.com>
2026-06-29 00:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
-  // write your code here
+  // write your code here		
+ let result = "";
+
+  for (let i = 0; i < l; i++) {
+    const index = Math.floor(Math.random() * chars.length);
+    result += chars[index];
+  }
+
+  return result;
 }
 
 // Do not change the code below.
